328b25815d911bc6811875f4ed00ef944144d8f0
Vinicius Sanchez <viniciuss.sanchez@gmail.com>
2024-07-18 11:55:10 -0300

    Bug fix export grids

--- src/DevExpress.CxGrid.Core.pas
+++ src/DevExpress.CxGrid.Core.pas
@@ -32,7 +32,16 @@ begin
     LSaveDialog.InitialDir := ExtractFilePath(ParamStr(0));
     if LSaveDialog.Execute then
     begin
-      ExportGridToXLSX(LSaveDialog.FileName, AGrid, AExpand);
+      case AFileType of
+        TDevExpress4DelphiFileType.EXCEL:
+          ExportGridToXLSX(LSaveDialog.FileName, AGrid, AExpand);
+        TDevExpress4DelphiFileType.TXT:
+          ExportGridToText(LSaveDialog.FileName, AGrid, AExpand);
+        TDevExpress4DelphiFileType.HTML:
+          ExportGridToHTML(LSaveDialog.FileName, AGrid, AExpand);
+        TDevExpress4DelphiFileType.XML:
+          ExportGridToXML(LSaveDialog.FileName, AGrid, AExpand);
+      end;
       if AOpenFileAfter then
         ShellExecute(0, 'open', pchar(LSaveDialog.FileName), nil, nil, SW_SHOW);
     end;
